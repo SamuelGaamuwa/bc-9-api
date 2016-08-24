4f9c4b922c874d3737487e5476646040bafbf238
SamuelGaamuwa <samuelgaamuwa@hotmail.com>
2016-08-24 12:05:45 +0300

    adds changes suggested by the TA, including returning a dictionary and proper formatting of the code

--- weather_api.py
+++ weather_api.py
@@ -1,24 +1,25 @@
 import urllib3
 import json
+
+http = urllib3.PoolManager()
+
 def weather_api(city):
 	try:
-		http = urllib3.PoolManager()
-
 		r = http.request(
-						'GET', 
-						'http://api.openweathermap.org/data/2.5/weather',
-						fields = {'q': city, 'units': 'metric', 'appid': '2bc3e79bb974a007818864813f53fd35' }
-						)
+				'GET', 
+				'http://api.openweathermap.org/data/2.5/weather',
+				fields = {'q': city, 'units': 'metric', 'appid': '2bc3e79bb974a007818864813f53fd35' }
+			)
 
 		data = json.loads(r.data.decode('utf-8'))
-		return [data['weather'][0]['description'], data['main']['temp']]
+		return {city: [data['weather'][0]['description'], data['main']['temp']]}
 	except Exception as e:
-		print (e)
+		return e
 
 
 print('{}\t{}\t{}'.format('City'.ljust(10), 'Description'.ljust(20), 'Temperature'.ljust(10)))
 print("=" * 50)
 for city in ['London', 'Nairobi', 'Kampala']:
-	print('{}\t{}\t{}'.format(city.ljust(10), weather_api(city)[0].ljust(20), str(weather_api(city)[1]).ljust(10)))
-
-
+	info = weather_api(city)
+	for item in info:
+		print ('{}\t{}\t{}'.format(city.ljust(10), str(info[city][0]).ljust(20), str(info[city][1]).ljust(10)))
